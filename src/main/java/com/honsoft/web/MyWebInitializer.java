72b27e837164dabaeb36d646e644c70e76fd39ae
changnam go <changnma.go@gmail.com>
2021-05-09 21:45:49 +0900

    initial commit

---
+++ src/main/java/com/honsoft/web/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package com.honsoft.web;
+
+import org.springframework.web.SpringServletContainerInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.honsoft.web.config.WebConfig;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		 return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
